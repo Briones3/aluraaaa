68b2f9f4a7765407bf3e33a51c750529db73ed1c
Briones <109561678+Briones3@users.noreply.github.com>
2024-07-15 15:21:00 -0300

    Add files via upload

---
+++ proyecto api/api/src/main/java/med/voll/api/direccion/DatosDireccion.java
@@ -0,0 +1,5 @@
+package med.voll.api.direccion;
+
+public record DatosDireccion(String calle, String barrio, String ciudad, Integer numero, String manzana, Integer casa) {
+
+}
---
+++ proyecto api/api/src/main/java/med/voll/api/medico/Especialidad.java
@@ -0,0 +1,12 @@
+package med.voll.api.medico;
+
+public enum Especialidad {
+
+
+    ORTOPEDIA,
+    CARDIOLOGIA,
+    GINECOLOGIA,
+    PEDIATRIA,
+    UROLOGIA
+
+}
